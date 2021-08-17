e8cd30cd8794e167fd71bc077c649975d03617b5
eccovskiy <eccovskiy@gmail.com>
2021-08-17 16:39:34 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/*let number = 6.4;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const person = '5';
+
+const bool = true;
+
+// console.log(Something);
+
+let und;
+
+console.log(und);
+
+const obj = {
+    name: 'Alex',
+    age: 25,
+    isMaried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+console.log(arr[1]);*/
+
+/*const ans = +prompt('Here?', '18+');
+console.log( ans + 5 );
+
+const answers = [];
+
+answers[0] = prompt('Your name', '');
+answers[1] = prompt('Your lastname', '');
+answers[2] = prompt('Your age', '');
+
+console.log(typeof (answers) );
+
+console.log(typeof (null) );*/
+
+/*const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Привет, ${user}`);*/
+
+/*console.log('arr' + ' - object');
+console.log(4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+++incr;
+--decr;
+
+console.log(incr++);
+console.log(--decr);
+
+console.log(2 + 2 * 2 !== '8');
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && !isClose);*/
