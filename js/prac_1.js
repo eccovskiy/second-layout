1bf7506620868684c3473d72ec76cc65e02ccf48
eccovskiy <eccovskiy@gmail.com>
2021-08-18 19:46:38 +0300

    Prac1 Upd

--- js/prac_1.js
+++ js/prac_1.js
@@ -1,20 +1,69 @@
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-    personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        privat: false
-    };
+let numberOfFilms;
+    
+function start () {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 console.log(personalMovieDB);
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = +prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 10) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            console.log('Error');
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersenalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count > 30) {
+        console.log('Вы киноман');
+    } else {
+        console('Error');
+    }
+}
+
+detectPersenalLevel();
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`);
+    }
+}
+
+writeYourGenres();
--- js/script.js
+++ js/script.js
@@ -66,3 +66,122 @@ const isChecked = true,
       isClose = false;
 
 console.log(isChecked && !isClose);*/
+
+/*if (4 == 9) {
+    console.log('Ok');
+} else {
+    console.log('Wrong');
+}
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Too much');
+} else {
+    console.log('Ok');
+}
+
+(num === 50) ? console.log('Ok') : console.log('Error');
+
+const num = '50';
+
+switch (num) {
+    case '49':
+        console.log('Error');
+        break;
+    case '100':
+        console.log('Error 2');
+        break;
+    case '50':
+        console.log('OK!');
+        break;
+    default:
+        console.log('Some going wrong');
+        break;
+} */
+
+/*let num = 50;
+
+while (num <= 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if (i == 6) {
+        // break;
+        continue;
+    }
+
+    console.log(i);
+}*/
+
+/*let num = 20;
+
+function showFirstMassage(text, arg) {
+    console.log(text + ' ' + arg);
+    let num = 10;
+    console.log(num);
+}
+
+showFirstMassage('Hello', 'World');
+console.log(num);
+
+function calc (a, b) {
+    return (a + b);
+}
+
+console.log(calc(5, 6));
+console.log(calc(15, 6));
+
+function ret () {
+    let num = 50;
+    return num;
+}
+
+let num2 = ret();
+
+console.log(num2);
+
+const logger = function() {
+    console.log('Hayushki');
+};
+
+logger();
+
+let calc = (a) => a + 5;
+
+console.log(calc(15));*/
+
+/*const str = 'teSt';
+const arr = [1, 2, 3];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+console.log(logg.slice(6, 11));
+console.log(logg.substring(6, 11));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = '12.2px';
+
+console.log(parseInt(test));
+console.log(parseFloat(test));*/
